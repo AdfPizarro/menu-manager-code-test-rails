f8c7e247e2513cc64ddd0cb0d72cb9171c886c5f
Adrian Flores Pizarro <adrian.pizarro@encora.com>
2022-02-07 08:42:37 -0700

    Add validation to prevent a menu cost to be exactly 77

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,14 +1,20 @@
 class Dish < ApplicationRecord
   validates :name, :price, presence: true
   validates :name, uniqueness: true
-  validate :nameNotStartWithE
+  validate :name_dont_start_with_e, :dont_be_77
   
-  
+
   belongs_to :menu
 
-  def nameNotStartWithE
+  def name_dont_start_with_e
     if (name.first=="e"||name.first=="E")
-      errors.add(:name, "Dish name can't start with E")
+      errors.add(:name, "Dish name can't start with E or 3")
+    end
+  end
+
+  def dont_be_77
+    if ( (menu.dishes.sum(:price)+price) == 77 )
+      errors.add(:name, "Menu price can't be 77")
     end
   end
 
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,5 @@
 class Menu < ApplicationRecord
     validates :name, presence: true
     has_many :dishes
+
 end
