a393bf537e085cb992ab04345cf1da822ad1e049
Adrian Flores Pizarro <adrian.pizarro@encora.com>
2022-02-07 17:33:45 -0700

    Add json response for the api menu

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   resources :dishes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  namespace :api do
+    namespace :v1 do
+      resources :menus,only: [:index], controller: 'api', defaults: { format: 'json' }
+
+    end
+  end
+
   # Defines the root path route ("/")
   root "menus#index"
 end
